94afe4194eec3fb95bf5cdef66fe8d64034de534
Marko Stevanovic <stevanovicmarko@gmail.com>
2026-09-07 14:24:41 +0200

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        Map<Character, Integer> taskMap = new HashMap<>();
+        for (char task : tasks) {
+            taskMap.merge(task, 1, Integer::sum);
+        }
+        if (taskMap.size() > n) {
+            return taskMap.values().stream()
+                      .mapToInt(Integer::intValue)
+                      .sum();
+        }
+
+        int mostFrequent = Collections.max(taskMap.values());
+        int maxCount = Collections.frequency(taskMap.values(), mostFrequent);
+        System.out.println(mostFrequent + " " + maxCount);
+        return ((mostFrequent-1) * (n+1)) + maxCount;
+    }
+}
